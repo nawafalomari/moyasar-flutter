5243074f17405772a8a5ff1749286cc8583d4b40
Nawaf <princenifam@gmail.com>
2024-07-11 16:56:29 +0300

    feat: implement tokenized card

---
+++ lib/src/models/sources/card/tokenized_card_source.dart
@@ -0,0 +1,29 @@
+import 'package:moyasar/src/models/payment_type.dart';
+import 'package:moyasar/src/models/sources/payment_request_source.dart';
+
+/// Required data to setup a Tokenized Credit Card payment.
+///
+/// Use only when you need to customize the UI.
+class CardPaymentRequestSource implements PaymentRequestSource {
+  @override
+  PaymentType type = PaymentType.token;
+
+  late String token;
+  late String threeDSecure;
+  late String manual;
+
+  CardPaymentRequestSource(
+      {required  this.token,
+      required  this.threeDSecure,
+      required bool manualPayment}) {
+      manual = manualPayment ? 'true' : 'false';
+  }
+
+  @override
+  Map<String, dynamic> toJson() => {
+        'type': type.name,
+        'token': token,
+        '3ds': threeDSecure,
+        'manual': manual
+      };
+}
